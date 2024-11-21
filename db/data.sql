a3478eb6c52d24543ffc2d584b84b282dd8d23ba
emmaGalzio <emmagalzio@gmail.com>
2024-11-21 00:09:21 -0300

    Configuracion docker compose

--- db/data.sql
+++ db/data.sql
@@ -1,4 +1,4 @@
 -- test rp
 -- insert into rp (id, name, description) values('auth-test.encodesa.com.ar', 'auth-test.encodesa.com.ar', 'example1');
 insert into rp (id, name, description) values('ox-pure-gorilla.ngrok-free.app', 'ox-pure-gorilla.ngrok-free.app', 'example1');
--- SELECT H2VERSION() FROM DUAL
+-insert into rp (id, name, description) values('elegant-cowbird-hopelessly.ngrok-free.app', 'elegant-cowbird-hopelessly.ngrok-free.app', 'example1');
